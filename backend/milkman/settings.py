cbcc95507d0fe68f780a993e566add3030ac4bb2
Anshika <anshikapriya12@gmail.com>
2026-03-17 00:24:41 +0530

    changes in django settings

--- backend/milkman/settings.py
+++ backend/milkman/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 CORS_ALLOW_ALL_ORIGINS = True
 
 # REST Framework settings
